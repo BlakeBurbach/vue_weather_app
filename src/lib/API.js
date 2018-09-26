4c30aff7997585f8b17f0e0947b1fda691e5a490
Blake Burbach <cras.bburbach@gmail.com>
2018-09-26 13:28:20 -0500

    got rid of vue linter errors

--- src/lib/API.js
+++ src/lib/API.js
@@ -6,11 +6,19 @@ const location = '37.8267,-122.4233';
 
 const API_URL = `${proxy}https://api.darksky.net/forecast/${API_KEY}/${location}`;
 
-const getWeather = async () => {
-    return fetch(API_URL)
+const GEO_API_URL = `${proxy}https://darksky.net/geo?=`;
+
+function getCoordinates() {
+  return fetch(`${GEO_API_URL}${location}`)
+    .then(response => response.json());
+}
+
+function getWeather() {
+  return fetch(API_URL)
     .then(response => response.json());
 }
 
 export default {
-    getWeather,
+  getWeather,
+  getCoordinates,
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,26 @@
 <template>
-  <div class="home">
-    <div class="row">
-      <div class="col-6 offset-3">
-        <div class="input-group mb-3">
-          <input type="text" class="form-control" placeholder="Enter Location" aria-label="Enter Location" aria-describedby="button-addon2">
-          <div class="input-group-append">
-            <button class="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
+  <div class='home'>
+    <div class='row'>
+      <div class='col-6 offset-3'>
+        <div class='input-group mb-3'>
+          <input v-model='location' type='text' class='form-control' placeholder='Enter Location'
+           aria-label='Enter Location' aria-describedby='button-addon2'>
+          <div class='input-group-append'>
+            <button @click='updateLocation' class='btn btn-outline-secondary' type='button'
+            id='button-addon2'>Search</button>
           </div>
         </div>
       </div>
-      <div class=" col-6 offset-3">
-        <div class="card text-white bg-primary mb-3">
-          <div class="card-header">Current Weather</div>
-            <div class="card-body text-center">
-              <h4 class="card-title">{{forecast.currently.summary}}</h4>
-              <div class="card-text">
-                <skycon v-bind:condition="forecast.currently.icon | uppercase"></skycon>
-                <span class="temp-text">{{forecast.currently.temperature}} ℉</span>
+      <div class=' col-6 offset-3' v-if='forecast'>
+        <div class='card text-white bg-primary mb-3'>
+          <div class='card-header'>Current Weather</div>
+            <div class='card-body text-center'>
+              <h4 class='card-title'>{{forecast.currently.summary}}</h4>
+              <div class='card-text'>
+                <skycon v-bind:condition='forecast.currently.icon | uppercase'></skycon>
+                <span class='temp-text'>{{forecast.currently.temperature}} ℉</span>
               </div>
-              <div class="card-text">
+              <div class='card-text'>
                 {{forecast.currently.precipProbability}}％ chance of precipitation
               </div>
             </div>
@@ -29,39 +31,40 @@
 </template>
 
 <script>
-import API from "@/lib/API.js";
-import Vue from "vue";
-import VueSkycons from "vue-skycons";
+import API from '@/lib/API';
+import Vue from 'vue';
+import VueSkycons from 'vue-skycons';
 
 // or pass the color option
-Vue.use(VueSkycons, { color: "white" });
+Vue.use(VueSkycons, { color: 'white' });
 
 export default {
-  name: "home",
+  name: 'home',
   components: {},
   data() {
     return {
-      forecast: {
-        currently: {
-          summary: String,
-          icon: String,
-          temperature: Number
-        }
-      }
+      location: '',
+      forecast: null,
     };
   },
   // when page loads, call the getWeather function from API
   mounted() {
-    API.getWeather().then(result => {
+    API.getWeather().then((result) => {
       console.log(result);
       this.forecast = result;
     });
   },
+  methods: {
+    updateLocation() {
+      API.getCoordinates(this.location).then((result) => {
+        console.log(result);
+      });
+    },
+  },
   filters: {
-    uppercase: function(v) {
-      return v.toUpperCase().replace(/-/g, "_");
-    }
-  }
+    uppercase: v => v.toUpperCase().replace(/-/g, '_')
+    ,
+  },
 };
 </script>
 
